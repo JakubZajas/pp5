3294aa1bcb3b93026f88f2b2730459337de20150
s-A301-05 <s-A301-05@CAMPUS.UEK.KRAKOW.PL>
2024-12-02 20:22:58 +0100

    zajecia

---
+++ my-app/src/invoice/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerFormComponent } from './components/customer-form/customer-form.component';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+
+
+const routes: Routes = [
+  {
+    path: 'customer-form',
+    component: CustomerFormComponent
+  },
+  {
+    path: 'customer-list',
+    component: CustomerListComponent
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
--- my-app/src/invoice/invoice.module.ts
+++ my-app/src/invoice/invoice.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerFormComponent } from './components/customer-form/customer-form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Routes } from '@angular/router';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
 
 
 
 @NgModule({
-  declarations: [CustomerFormComponent],
-  exports: [CustomerFormComponent],
+  declarations: [],
+  exports: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class InvoiceModule { }
